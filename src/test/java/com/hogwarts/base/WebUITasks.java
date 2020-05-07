1c121c2df71fa0b71a19d8bb480d1c55e845cd6e
Richard <G_ALLEN@163.com>
2020-05-07 18:14:38 +0800

    20200507commit

--- src/test/java/com/hogwarts/base/WebUITasks.java
+++ src/test/java/com/hogwarts/base/WebUITasks.java
@@ -5,12 +5,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 /**
  * Created by JiZhi.Qian on 2019/3/11.
  */
 public class WebUITasks {
     private static int waitShot = 30;
     private static Logger logger = Logger.getLogger(WebUITasks.class);
+
+    public static void input(By by, String txt, WebDriver driver) throws Exception{
+        WebElement ele = driver.findElement(by);
+        ele.sendKeys(txt);
+        logger.info("input text: " + txt);
+    }
+
+    public static void click(By by, WebDriver driver) throws Exception{
+        WebElement ele = driver.findElement(by);
+        ele.click();
+        logger.info("click: " + by.toString());
+    }
+
+    public static void clickTheElement(By by, int index, WebDriver driver) throws Exception{
+        List<WebElement> eles = driver.findElements(by);
+        WebElement ele = eles.get(index);
+        ele.click();
+        logger.info("click: " + ele.toString());
+    }
+
+    public static void clickTheElement(By by, String txt, WebDriver driver) throws Exception{
+        List<WebElement> eles = driver.findElements(by);
+        for (int i = 0; i < eles.size(); i++) {
+            WebElement e = eles.get(i);
+            if (e.getText().equals(txt))
+                e.click();
+            logger.info("click: " + e.toString());
+        }
+    }
 
     public static void inputText(String txt, WebDriver driver) throws Exception{
         WebElement searchInput = findElementByXpath("//input[@name='wd' and @id='kw']",driver);
