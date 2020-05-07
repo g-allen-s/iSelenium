1f386ceb389b5f91fbf02319daa8778edb3b9425
Richard <G_ALLEN@163.com>
2020-05-07 18:23:10 +0800

    20200507commit

--- src/test/java/com/hogwarts/webui/TestBaiDu.java
+++ src/test/java/com/hogwarts/webui/TestBaiDu.java
@@ -4,50 +4,51 @@ import com.hogwarts.base.*;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
-/**
- * Created by JiZhi.Qian on 2019/3/11.
- */
+
 public class TestBaiDu extends WebUIBase {
     private Logger logger = Logger.getLogger(TestBaiDu.class);
 
-    @Test
-    public void baiduSearch1() throws Exception{
-        String checkString = "selenium";
-        baiduSearch(checkString);
-    }
+    private static int testByTE = 0;
+    private static int testByPD = 1;
+
 
     @Test
-    public void baiduSearch2() throws Exception{
-        String checkString = "Appium";
-        baiduSearch(checkString);
+    public void cmdcGo() throws Exception{
+        logger.info("Go to cmdc and fuck it up.");
+        goCmdc("ST200430027550", 0);
     }
 
-    /**
-     * 百度搜索公共子函数
-     *
-     * @param checkString 搜索关键词
-     * @throws Exception
-     */
-    public void baiduSearch(String checkString) throws Exception {
-        testcaseName = "Baidu search UI automation test.";
-
-        logger.info("Start the automation test: " + testcaseName);
-
-        //浏览器中打开百度
-        logger.info("Open the www.baidu.com");
-        navigation.to("http://www.baidu.com");
+    public void goCmdc(String cmdcCode, int testBy) throws Exception {
+        // 打开浏览器
+        String url = "http://cmdc.cnsuning.com/cmdc/demandManage/demandInfo.action?brCode=" + cmdcCode;
+        logger.info("Open the fucking cmdc");
+        navigation.to(url);
         wait2s();
-
-        //输入搜索内容：Selenium
-        WebUITasks.inputText(checkString,driver);
+        // 登录
+        WebUITasks.input(By.id("userId"), "12010077", driver);
+        WebUITasks.input(By.id("password"), "Sn@197777", driver);
+        WebUITasks.click(By.id("submit_btn"), driver);
         wait2s();
-
-        //单击搜索按钮
-        WebUITasks.clickSearchBtn(driver);
+        // 【测试确认】
+        WebUITasks.click(By.className("popTeConf"), driver);
+        // 测试环境
+        WebUITasks.clickTheElement(By.className("default-val"), 11, driver);
+        WebUITasks.clickTheElement(By.tagName("span"), "sit/pre", driver);
+        // 压测环境
+        WebUITasks.clickTheElement(By.className("default-val"), 12, driver);
+        WebUITasks.clickTheElement(By.tagName("span"), "无压测", driver);
+        if (testBy == testByPD) {
+            // 是否产品自测
+            WebUITasks.clickTheElement(By.className("default-val"), 13, driver);
+            WebUITasks.clickTheElement(By.tagName("span"), "是", driver);
+            // 产品自测原因
+            WebUITasks.clickTheElement(By.className("default-val"), 14, driver);
+            WebUITasks.clickTheElement(By.tagName("span"), "其它", driver);
+        }
+        // 【确定】
+//        WebUITasks.click(By.className("popWinBtnYes"), driver);
         wait2s();
-
-        String browserTitle = driver.getTitle();
-        Assert.assertTrue("Check if the web page contains the key word ‘" + checkString + "’", browserTitle.contains(checkString));
     }
 }
