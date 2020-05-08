0b0369614c0c4f31056fcbf0b691d2c08a9d9000
Richard <G_ALLEN@163.com>
2020-05-08 15:08:12 +0800

    20200508commit

--- src/test/java/com/my/webui/RunCmdc.java
+++ src/test/java/com/my/webui/RunCmdc.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 
 
-public class RunCmdc extends WebUIBase {
+public class RunCmdc extends BaseTest {
     private Logger logger = Logger.getLogger(RunCmdc.class);
 
     private static int testByTE = 0;
@@ -15,46 +15,48 @@ public class RunCmdc extends WebUIBase {
 
     @Test
     public void cmdcGo() throws Exception{
-        logger.info("Go to cmdc and fuck it up.");
         setParamter("cmdcCode");
         setParamter("testBy");
-        goCmdc(String.valueOf(paramters.get("cmdcCode")),
-                Integer.parseInt(String.valueOf(paramters.get("testBy"))));
+        String cmdcCodes = String.valueOf(paramters.get("cmdcCode"));
+        if (cmdcCodes == null || cmdcCodes.equalsIgnoreCase("")) return;
+        String[] codes = cmdcCodes.split(",");
+
+        String testBys = String.valueOf(paramters.get("testBy"));
+        if (testBys == null || testBys.equalsIgnoreCase("")) return;
+        String[] bys = testBys.split(",");
+
+        for(int i = 0; i < codes.length; i++) {
+            goCmdc(codes[i], Integer.parseInt(bys[i]));
+        }
     }
 
     public void goCmdc(String cmdcCode, int testBy) throws Exception {
+        logger.info("Go to " + cmdcCode + " and fuck it up.");
         // 打开浏览器
-        String url = "http://cmdc.cnsuning.com/cmdc/demandManage/demandInfo.action?brCode=" + cmdcCode;
-        logger.info("Open the fucking cmdc");
+        String url = prop.getProperty("CMDC_URL") + "=" + cmdcCode;
         navigation.to(url);
         wait2s();
-        // 登录
-        WebUITasks.input(By.id("userId"), "12010077", driver);
-        WebUITasks.input(By.id("password"), "Sn@197777", driver);
-        WebUITasks.click(By.id("submit_btn"), driver);
-        logger.info("Login successfully.");
-        wait2s();
         // 【测试确认】
-        WebUITasks.click(By.className("popTeConf"), driver);
+        BasePage.click(By.className("popTeConf"), driver);
         // 测试环境
-        WebUITasks.clickTheElement(By.className("default-val"), 11, driver);
-        WebUITasks.clickTheElement(By.tagName("span"), "sit/pre", driver);
+        BasePage.clickTheElement(By.className("default-val"), 11, driver);
+        BasePage.clickTheElement(By.tagName("span"), "sit/pre", driver);
         // 压测环境
-        WebUITasks.clickTheElement(By.className("default-val"), 12, driver);
-        WebUITasks.clickTheElement(By.tagName("span"), "无压测", driver);
+        BasePage.clickTheElement(By.className("default-val"), 12, driver);
+        BasePage.clickTheElement(By.tagName("span"), "无压测", driver);
         if (testBy == testByTE) {
             // do nothing
         }
         if (testBy == testByPD) {
             // 是否产品自测
-            WebUITasks.clickTheElement(By.className("default-val"), 13, driver);
-            WebUITasks.clickTheElement(By.tagName("span"), "是", driver);
+            BasePage.clickTheElement(By.className("default-val"), 13, driver);
+            BasePage.clickTheElement(By.tagName("span"), "是", driver);
             // 产品自测原因
-            WebUITasks.clickTheElement(By.className("default-val"), 14, driver);
-            WebUITasks.clickTheElement(By.tagName("span"), "其它", driver);
+            BasePage.clickTheElement(By.className("default-val"), 14, driver);
+            BasePage.clickTheElement(By.tagName("span"), "其它", driver);
         }
         // 【确定】
-        WebUITasks.click(By.className("popWinBtnYes"), driver);
+//        WebUITasks.click(By.className("popWinBtnYes"), driver);
         wait2s();
         logger.info("[[" + cmdcCode + "]] Mission complicated.");
     }
